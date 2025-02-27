ae7e19da4d2cbade5d63cae0cae708769aee4c9c
vikenty19 <vikenty.plakhov@testpro.io>
2025-02-27 16:51:02 -0500

    .

---
+++ src/test/java/Runners/TestRunner.java
@@ -0,0 +1,37 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        features = {"src/test/resources/features/Register.feature"},
+        glue = {"StepDefinitions"},
+        plugin = {"pretty","html:target/cucomber-reports.html"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+
+    @BeforeClass(alwaysRun = true)
+    public void setUpCucumber() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideScenarios();
+    }
+
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() {
+        testNGCucumberRunner.finish();
+    }
+
+}
+
