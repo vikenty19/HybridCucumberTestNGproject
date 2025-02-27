594314fa7500641885733a95729e3590c8d3d1c0
vikenty19 <vikenty.plakhov@testpro.io>
2025-02-27 16:30:48 -0500

    StepDefinitions added

--- src/test/java/StepDefinitions/RegisterSteps.java
+++ src/test/java/StepDefinitions/RegisterSteps.java
@@ -1,4 +1,32 @@
 package StepDefinitions;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class RegisterSteps {
+    @Given("I launch the app")
+    public void iLaunchTheApp() {
+    }
+
+    @And("I navigate to Account Registration page")
+    public void iNavigateToAccountRegistrationPage() {
+    }
+
+    @When("I provide all the below valid details :")
+    public void iProvideAllTheBelowValidDetails() {
+    }
+
+    @And("I check-in the Privacy Policy")
+    public void iCheckInThePrivacyPolicy() {
+    }
+
+    @And("I click on continue button")
+    public void iClickOnContinueButton() {
+    }
+
+    @Then("I should see that the User Account has successfully been created")
+    public void iShouldSeeThatTheUserAccountHasSuccessfullyBeenCreated() {
+    }
 }
