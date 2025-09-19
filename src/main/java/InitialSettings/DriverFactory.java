28128af41f5bae223e7ffc63163485596dc50094
vikenty19 <vikenty.plakhov@testpro.io>
2025-09-19 17:13:30 -0400

    Edge driver loading from my comp

--- src/main/java/InitialSettings/DriverFactory.java
+++ src/main/java/InitialSettings/DriverFactory.java
@@ -26,7 +26,10 @@ public class DriverFactory {
                 driver = new FirefoxDriver();
                 break;
             case "edge":
-                WebDriverManager.edgedriver().driverVersion("140.0.0.0").setup();
+            //    WebDriverManager.edgedriver().driverVersion("140.0.0.0").setup();
+
+                 String edgePath = prop.getProperty("edge.driver.path");
+                System.setProperty("webdriver.edge.driver", edgePath);
                 driver = new EdgeDriver();
                 break;
             default:
